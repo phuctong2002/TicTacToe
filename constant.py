1860c301aa4da095a8769e409f7fe98a9cf25279
phuctong2002 <vanphuc15092002@gmail.com>
2023-06-22 23:42:56 +0700

    edit

--- constant.py
+++ constant.py
@@ -6,7 +6,7 @@ HEIGHT = 600
 step = 0
 # step chan la nguoi di le la may di
 matrix = []
-# 0 la chua di 1 la nguoi di 2 la may di
+# 0 la chua di 1 la may di -1 la may nguoi
 # chien luoc la may thang tuong duong voi viec nguoi chon min may chon max
 for i in range(10):
     row = []
